b3be3c3737f86704f464378974883d84b31cba87
zhouning <ncutits@163.com>
2017-10-09 18:17:21 +0800

    no message

--- log-analysis/etl/command/etl.py
+++ log-analysis/etl/command/etl.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys,time
+import sys, time
 
 
 def timestamp_datetime(value):
     format = '%Y-%m-%d %H:%M:%S'
     value = time.localtime(value)
-    dt = time.strftime(format,value)
+    dt = time.strftime(format, value)
     return dt
+
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
@@ -21,5 +22,6 @@ for line in sys.stdin:
     for i in range(len(columns)):
         columns[i] = columns[i].strip()
 
-    columns[3]=timestamp_datetime(int(columns[3]))
-    print columns[0]+","+columns[1]+","+columns[2]+","+columns[3]+","+columns[4]+","+columns[5]+","+columns[6]+","+columns[7]+","+columns[8]+","+columns[9]
+    columns[3] = timestamp_datetime(int(columns[3]))
+    print columns[0] + "," + columns[1] + "," + columns[2] + "," + columns[3] + "," + columns[4] + "," + columns[
+        5] + "," + columns[6] + "," + columns[7] + "," + columns[8] + "," + columns[9]
